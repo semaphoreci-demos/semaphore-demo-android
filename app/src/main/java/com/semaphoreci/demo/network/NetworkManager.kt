97c4354553dd6c89a05fdd9021a3c3327d5fa960
biklas7 <antonio@coletiv.com>
2020-07-14 15:49:24 +0100

    added network manager

---
+++ app/src/main/java/com/semaphoreci/demo/network/NetworkManager.kt
@@ -0,0 +1,27 @@
+package com.semaphoreci.demo.network
+
+import com.google.gson.GsonBuilder
+import com.haroldadmin.cnradapter.NetworkResponseAdapterFactory
+import com.semaphoreci.demo.network.remotedatasource.RepoRemoteDataSource
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkManager {
+    private const val baseUrl = "api.github.com"
+
+    private val gson = GsonBuilder()
+        .setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        .create()
+
+    private val client: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://$baseUrl")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(NetworkResponseAdapterFactory())
+            .build()
+    }
+
+    val repoRemoteDataSource: RepoRemoteDataSource by lazy {
+        client.create(RepoRemoteDataSource::class.java)
+    }
+}
