a144ba4cffe5cf0790260b392e45940870cb9ad0
biklas7 <antonio@coletiv.com>
2020-07-14 12:14:19 +0100

    enforce line length of 80

--- app/src/androidTest/java/com/semaphoreci/demo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/semaphoreci/demo/ExampleInstrumentedTest.kt
@@ -16,7 +16,8 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val appContext =
+            InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.semaphoreci.demo", appContext.packageName)
     }
 }
