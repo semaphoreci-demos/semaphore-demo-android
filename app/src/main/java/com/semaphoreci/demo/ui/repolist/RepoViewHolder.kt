d0eb092ac0fcd261e7e9fc72b275a3446e4f2d66
biklas7 <antonio@coletiv.com>
2020-07-15 18:12:30 +0100

    added repo details screen

--- app/src/main/java/com/semaphoreci/demo/ui/repolist/RepoViewHolder.kt
+++ app/src/main/java/com/semaphoreci/demo/ui/repolist/RepoViewHolder.kt
@@ -1,8 +1,10 @@
 package com.semaphoreci.demo.ui.repolist
 
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import com.semaphoreci.demo.databinding.ViewRepoBinding
 import com.semaphoreci.demo.network.models.Repo
+import com.semaphoreci.demo.ui.repodetails.RepoDetailsActivity
 
 class RepoViewHolder(
     private val viewRepoBinding: ViewRepoBinding
@@ -10,12 +12,19 @@ class RepoViewHolder(
 
     fun bind(repo: Repo) = viewRepoBinding.apply {
         repoName.text = repo.name
-        repoLanguage.text = repo.language
+        repoLanguage.text = repo.language ?: ""
         repoForkCount.text = repo.forksCount.toString()
         repoStarsCount.text = repo.stargazersCount.toString()
         repoWatcherCount.text = repo.watchersCount.toString()
         repoIssueCount.text = repo.openIssuesCount.toString()
 
-        repoCard.setOnClickListener { }
+        repoCard.setOnClickListener {
+            with(viewRepoBinding.root.context) {
+                val repoDetailsIntent =
+                    Intent(this, RepoDetailsActivity::class.java)
+                repoDetailsIntent.putExtra("REPO", repo)
+                startActivity(repoDetailsIntent)
+            }
+        }
     }
 }
